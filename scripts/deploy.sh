7cd5f26297cbc72e73220020cf3b804466a0d125
Jacob Garcia <jacob.garcia@codewilling.com>
2018-11-07 15:22:50 -0600

    stage 2

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo " ----- clone app repo ----- "
+git clone https://github.com/Artemmkin/raddit.git
+
+echo " ----- install dependent gems ----- "
+cd .raddit
+sudo bundle install
+
+echo " ----- start the app ----- "
+sudo systemctl start raddit
+sudo systemctl enable raddit
